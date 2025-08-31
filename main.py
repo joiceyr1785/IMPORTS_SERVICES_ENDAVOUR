b90e6fdee3dc18865457ead7072e34bf92cdafec
Joice Yllesca <joiceyr1785@gmail.com>
2025-08-31 03:18:15 -0500

    BD

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hola mundo ")
+import pyodbc
+import pandas as pd
+
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=USUARIO-MKQRT2U;'
+    'DATABASE=IMPORTS_SERVICES_ENDAVOUR;'
+    'Trusted_Connection=yes'
+)
+
+query = "SELECT * FROM Hecho_Licencias_Obras;"
+df = pd.read_sql(query, conn)
+
+print(df)
